c2a5a31b28d78c3ac0b32bcbffb9a8e6616c1b56
Krisna Bimantoro <krisnabmntr@gmail.com>
2024-08-06 08:18:34 +0700

    updated

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,10 @@ import db from "./utils/database";
 import routes from "./route";
 import bodyParser from "body-parser";
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -11,7 +15,12 @@ db();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 app.use("/api", routes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
