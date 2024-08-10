33adcd0da9a4b7226d76cdecd7c4bbabadf56afc
Krisna Bimantoro <krisnabmntr@gmail.com>
2024-08-10 14:06:08 +0700

    updated

--- src/utils/encryption.ts
+++ src/utils/encryption.ts
@@ -1,6 +1,5 @@
 import crypto from "crypto";
 
-// Function to ensure the key is 32 bytes long
 const ensureKeyLength = (key: string): Buffer => {
   if (key.length !== 32) {
     throw new Error("Key length must be 32 bytes.");
@@ -10,21 +9,19 @@ const ensureKeyLength = (key: string): Buffer => {
 
 export const encrypt = (key: string, plainText: string): string => {
   const iv = crypto.randomBytes(16);
-  const validKey = ensureKeyLength(key);
-  const cipher = crypto.createCipheriv("aes-256-ctr", validKey, iv);
+  const cipher = crypto.createCipheriv("aes-256-ctr", Buffer.from(key), iv);
   let encrypted = cipher.update(plainText);
   encrypted = Buffer.concat([encrypted, cipher.final()]);
   return `${iv.toString("hex")}:${encrypted.toString("hex")}`;
 };
 
-export const decrypt = (key: string, encryptedText: string): string => {
-  const textParts = encryptedText.split(":");
+export const decrypt = (key: string, plainText: string) => {
+  const textParts = plainText.split(":");
   const ivHex = textParts.shift();
   const iv = ivHex ? Buffer.from(ivHex, "hex") : Buffer.alloc(0);
-  const validKey = ensureKeyLength(key);
-  const encryptedBuffer = Buffer.from(textParts.join(":"), "hex");
-  const decipher = crypto.createDecipheriv("aes-256-ctr", validKey, iv);
-  let decrypted = decipher.update(encryptedBuffer);
+  const encryptedText = Buffer.from(textParts.join(":"), "hex");
+  const decipher = crypto.createDecipheriv("aes-256-ctr", Buffer.from(key), iv);
+  let decrypted = decipher.update(encryptedText);
   decrypted = Buffer.concat([decrypted, decipher.final()]);
   return decrypted.toString();
 };
